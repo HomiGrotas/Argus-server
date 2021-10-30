2847edd2e7d574d62ab0b8109ae8ab055aa5ef61
NadavShani <shaninadav10@gmail.com>
2021-10-31 00:32:41 +0300

    parent_token -> access_token

--- app/resources/child/args_handlers.py
+++ app/resources/child/args_handlers.py
@@ -3,7 +3,7 @@ from flask_restful import reqparse
 from app.resources.utils import mac_address_validator
 
 
-child_registration = reqparse.RequestParser(bundle_errors=True)
+child_registration = reqparse.RequestParser()
 child_registration.add_argument('mac_address', required=True, type=mac_address_validator, location='json')
 child_registration.add_argument('nickname', required=True, type=str, location='json')
-child_registration.add_argument('parent_token', required=True, location='json')  # JWT token for attaching to parent
+child_registration.add_argument('access_token', required=True, location='json')  # JWT token for attaching to parent
