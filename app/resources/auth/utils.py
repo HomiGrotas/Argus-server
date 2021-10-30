98ed83500448148e06c9020ea2af20c888b62301
NadavShani <shaninadav10@gmail.com>
2021-10-31 00:24:47 +0300

    split auth to auth/utils.py useful functions moved to utils

---
+++ app/resources/auth/utils.py
@@ -0,0 +1,28 @@
+from validators import mac_address, email, ValidationFailure
+from collections import namedtuple
+
+User = namedtuple('User', ['user', 'type'])
+
+
+def valid_email(user_email: str) -> bool:
+    """
+        custom email validator
+    """
+    try:
+        email(user_email)
+    except ValidationFailure:
+        return False
+    else:
+        return True
+
+
+def valid_mac_address(user_mac_address: str) -> bool:
+    """
+        custom mac address validator
+    """
+    try:
+        mac_address(user_mac_address)
+    except ValidationFailure:
+        return False
+    else:
+        return True
