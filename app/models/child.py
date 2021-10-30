f38067a927c2bb91e30692c22c988766408f3ba4
NadavShani <shaninadav10@gmail.com>
2021-10-31 00:33:31 +0300

    added info and tokens methods generate and verify token

--- app/models/child.py
+++ app/models/child.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, JSON, BOOLEAN, String, ForeignKey
 from flask_jwt_extended import create_access_token, get_jwt_identity
-
+from hmac import compare_digest
+from secrets import token_urlsafe
 
 from app import db
 
@@ -10,11 +11,11 @@ class Child(db.Model):
     __tablename__ = 'Children'
 
     id = Column(Integer, primary_key=True)
-    parent_id = Column(Integer, ForeignKey('Parents.id'), nullable=False)
 
+    parent_id = Column(Integer, ForeignKey('Parents.id'), nullable=False)
     mac_address = Column(String, unique=True, nullable=False)
-    token = Column(String, unique=True, nullable=False)
-    nickname = Column(String, unique=True)
+    token = Column(String, nullable=False)
+    nickname = Column(String, nullable=False)
 
     usage_limits = Column(JSON)       # {day: amount}
     block_websites = Column(String)   # List will be parsed to '[x, y, z]'
@@ -27,3 +28,18 @@ class Child(db.Model):
     def generate_auth_token(self):
         additional_claims = {"type": "child"}
         return create_access_token(identity=self.id, additional_claims=additional_claims)
+
+    def verify_token(self, token):
+        """ compares users token to the given one. Safe from timing attacks """
+        return compare_digest(token, self.token)
+
+    def info(self):
+        return {
+            'id': self.id,
+            'nickname': self.nickname,
+        }
+
+    @staticmethod
+    def generate_token(n_bytes=64):
+        """ token (64*8=) 512 bits to prevent brute-force attacks """
+        return token_urlsafe(n_bytes)
