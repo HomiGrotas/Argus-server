e7ea16fb66ef2d9c6a1488ea227077e65126f8d7
NadavShani <shaninadav10@gmail.com>
2021-10-31 00:29:26 +0300

    user is a named tuple and users can be identified in auth by role (parent/ child)

--- app/resources/parent/parent.py
+++ app/resources/parent/parent.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 from http import HTTPStatus
 from flask import g
 
-from app.resources.parent.args_handlers import parent_registration_parser, parent_updater_parser
+from .args_handlers import parent_registration_parser, parent_updater_parser
 from app.resources import exceptions
 from app import models, db, auth
 
@@ -18,9 +18,8 @@ class Parent(Resource):
         if models.Parent.query.filter_by(email=email).first() is not None:
             raise exceptions.EmailAlreadyTaken
 
-        parent = models.Parent(email=email)
+        parent = models.Parent(email=email, password_hash=models.Parent.hash_password(password))
         parent.nickname = nickname
-        parent.password_hash = parent.hash_password(password)
 
         try:
             db.session.add(parent)
@@ -34,34 +33,31 @@ class Parent(Resource):
 
     @auth.login_required
     def get(self):
-        if g.user_type == models.UsersTypes.parent.name:
-            info = g.user.info()
+        if g.user.type == models.UsersTypes.parent.name:
+            parent = g.user.user
         else:
-            parent = Parent.query.get(g.user.parent_id)
-            info = parent.info()
-        return info, HTTPStatus.OK
+            parent = models.Parent.query.get(g.user.user.parent_id)
+        return parent.info(), HTTPStatus.OK
 
-    @auth.login_required
+    @auth.login_required(role=models.UsersTypes.parent.name)
     def patch(self):
-        if g.user_type != models.UsersTypes.parent.name:
-            raise exceptions.NotAuthorized
         args = parent_updater_parser.parse_args()
 
         for key, value in args.items():
             if value:
                 if key == 'password':
-                    setattr(g.user, 'password_hash', g.user.hash_password(value))
+                    setattr(g.user.user, 'password_hash', g.user.user.hash_password(value))
                     continue
-                if key == 'email' and value != g.user.email:
+                if key == 'email' and value != g.user.user.email:
                     if models.Parent.query.filter_by(email=value).first() is not None:  # email doesn't exists
                         raise exceptions.EmailAlreadyTaken
-                    setattr(g.user, key, value)
+                    setattr(g.user.user, key, value)
                     continue
 
-                setattr(g.user, key, value)
+                setattr(g.user.user, key, value)
 
         try:
-            db.session.add(g.user)
+            db.session.add(g.user.user)
             db.session.commit()
 
         except Exception as e:
@@ -69,4 +65,4 @@ class Parent(Resource):
             db.session.rollback()
             raise exceptions.InternalServerError
 
-        return g.user.info(), HTTPStatus.OK
+        return g.user.user.info(), HTTPStatus.OK
