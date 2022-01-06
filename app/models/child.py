3893ab549b21f04be44526207b0404ad72009184
homigrotas <shaninadav10@gmail.com>
2022-01-06 20:32:05 +0200

    added properties

--- app/models/child.py
+++ app/models/child.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, JSON, BOOLEAN, String, ForeignKey
 from hmac import compare_digest
 from secrets import token_urlsafe
+from datetime import datetime
 
 from app import db
 from app.resources import exceptions
@@ -17,9 +18,9 @@ class Child(db.Model):
     _mac_address = Column(String(17), unique=True, nullable=False)
     token = Column(String(154), nullable=False)
     _nickname = Column(String(20), nullable=False, unique=True)
-    blocked = Column(BOOLEAN, default=False)
+    _blocked = Column(BOOLEAN, default=False)
 
-    usage_limits = Column(JSON, default={
+    _usage_limits = Column(JSON, default={
         'sunday': 0, 'monday': 0, 'tuesday': 0, 'wednesday': 0, 'thursday': 0, 'friday': 0, 'saturday': 0})       # {day: amount}
 
     block_websites = relationship("BlockedWebsites", secondary=child_blocked_websites)  # MTM with blocked websites
@@ -32,7 +33,7 @@ class Child(db.Model):
         return self._mac_address
 
     @mac_address.setter
-    def mac_address(self, mac_address):
+    def mac_address(self, mac_address: str):
         # child can't have same mac address or nickname
         if Child.query.filter_by(_mac_address=mac_address).first() is not None:
             raise exceptions.ChildMacAlreadyExists
@@ -49,6 +50,29 @@ class Child(db.Model):
             raise exceptions.NicknameAlreadyExists
         self._nickname = nickname
 
+    @property
+    def blocked(self):
+        return self._blocked
+
+    @blocked.setter
+    def blocked(self, block: bool):
+        if not isinstance(block, bool):
+            raise TypeError("Blocked must be boolean")
+        self._blocked = block
+
+    @property
+    def usage_limits(self):
+        return self._usage_limits
+
+    @usage_limits.setter
+    def usage_limits(self, new_limits: dict):
+        for key, val in new_limits:
+            if key not in self.usage_limits:
+                raise KeyError(f"{key} is not an acceptable day")
+            elif val < 0:
+                raise ValueError(f"Limit must be a positive number. Got: {val}")
+            self.usage_limits[key] = val
+
     def verify_token(self, token):
         """ compares users token to the given one. Safe from timing attacks """
         return compare_digest(token, self.token)
@@ -57,8 +81,8 @@ class Child(db.Model):
         return {
             'id': self.id,
             'parent_id': self.parent_id,
-            'mac_address': self._mac_address,
-            'nickname': self._nickname,
+            'mac_address': self.mac_address,
+            'nickname': self.nickname,
             'blocked': self.blocked,
             'usage_limits': self.usage_limits,
             'block_websites': self.block_websites,
