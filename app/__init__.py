5d005b9c2ea32f94c895a780a1417073dfd0ca80
homigrotas <shaninadav10@gmail.com>
2022-01-06 20:31:55 +0200

    added flask shell

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from app.resources.exceptions import errors
 db = SQLAlchemy()
 restful = Api(errors=errors)
 
-auth = HTTPBasicAuth()  #  todo: change to digest_auth
+auth = HTTPBasicAuth()  # todo: change to digest_auth
 
 
 def create_app():
@@ -32,6 +32,16 @@ def create_app():
 
 
 flask_app = create_app()
+
+
+# define the shell context
+@flask_app.shell_context_processor
+def shell_context():  # pragma: no cover
+    from app import models
+
+    ctx = {'db': db, 'models': models}
+    return ctx
+
 
 if __name__ == '__main__':
     flask_app.run()
