1b56afd75d07c6577013d5ef092c288c48c84d88
homigrotas <shaninadav10@gmail.com>
2022-01-13 22:41:44 +0200

    added commands template

--- app/resources/BlockedWebsites/args_handlers.py
+++ app/resources/BlockedWebsites/args_handlers.py
@@ -4,10 +4,8 @@ from app.models.utils.validators import domain_validator
 
 get_blocked_parser = reqparse.RequestParser()
 get_blocked_parser.add_argument('child_nickname', type=str, required=True)
-get_blocked_parser.add_argument('amount', type=int, default=10)
 
-post_blocked_parser = reqparse.RequestParser()
-post_blocked_parser.add_argument('child_id', type=int, required=True)
+post_blocked_parser = get_blocked_parser.copy()
 post_blocked_parser.add_argument('domain', type=domain_validator, required=True)
 
 delete_blocked_parser = post_blocked_parser.copy()
--- app/resources/commands/args_handlers.py
+++ app/resources/commands/args_handlers.py
@@ -1,1 +1,9 @@
 from flask_restful import reqparse
+
+get_commands = reqparse.RequestParser()
+get_commands.add_argument('child_nickname')
+
+post_command = get_commands.copy()
+post_command.add_argument('command')
+
+delete_command = post_command.copy()
