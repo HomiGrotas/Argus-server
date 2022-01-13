d2e7e905ac5fbe099200beda336022105a57dd19
homigrotas <shaninadav10@gmail.com>
2022-01-13 22:42:24 +0200

    added commands template

---
+++ app/resources/commands/commands.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+# todo: which commands are available (custom + cmd?)
+
+
+class Commands(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
