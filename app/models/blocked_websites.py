e1eec2ab7a53d179e91a894e517f5387075d705c
homigrotas <shaninadav10@gmail.com>
2022-01-17 12:16:52 +0200

    added levels

--- app/models/blocked_websites.py
+++ app/models/blocked_websites.py
@@ -2,16 +2,16 @@ from sqlalchemy import Column, Integer, String
 
 from app import db
 
-# todo: add levels (1-5)
-
 
 class BlockedWebsites(db.Model):
     __tablename__ = "BlockedWebsites"
     id = Column(Integer, primary_key=True)
     domain = Column(String(253), nullable=False)
+    level = Column(Integer, nullable=False, default=0)
 
     def info(self):
         return {
             'id': self.id,
             'domain': self.domain,
+            'level': self.level,
         }
