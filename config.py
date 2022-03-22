f1c6bedc31b8666fb250f56ad521556ab89d9e9b
homigrotas <shaninadav10@gmail.com>
2022-03-22 23:13:21 +0200

    added sqlite db

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ EXPIRATION_MIN = 2
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe(256)
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.environ.get('MySQL_Username')}:{os.environ.get('MySQL_Password')}@localhost/db"
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.environ.get('MySQL_Username')}:{os.environ.get('MySQL_Password')}@localhost/db"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=EXPIRATION_MIN)    # disable expiration
     DEBUG = True
