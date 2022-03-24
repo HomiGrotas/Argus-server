098cd19b1c4d8e9c6ddc70f25df7f4a44cbaa995
homigrotas <shaninadav10@gmail.com>
2022-03-24 18:29:55 +0200

    more postgres support

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 EXPIRATION_MIN = 2
 
 
+def get_postgres():
+    path = os.environ.get("DATABASE_URL")
+    if not path:
+        raise FileNotFoundError("Couldn't find DATABASE_URL")
+    return path.replace("postgres://", "postgresql://", 1)
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe(256)
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
