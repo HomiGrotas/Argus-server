4778622988607517450daf57762fe0d2cda6e05a
homigrotas <shaninadav10@gmail.com>
2022-06-15 21:25:43 +0300

    changed popularity to opened only

--- app/resources/child/statistics/popularity/popularity.py
+++ app/resources/child/statistics/popularity/popularity.py
@@ -1,8 +1,7 @@
 from flask_restful import Resource
 from flask import g
-from sqlalchemy import func, desc
+from sqlalchemy import func, desc, and_
 
-from app.resources import exceptions
 from app import models, auth, db
 from app.models.utils.decorators import safe_db
 
@@ -13,17 +12,21 @@ class Popularity(Resource):
     def get(self):
         children = [child.id for child in g.user.user.children]
 
-        # get most popular apps of the parent children
-        histories = db.session.query(
-            models.AppsHistory._name,
-            func.count(models.AppsHistory.id).label('qty'))\
-            .filter(models.AppsHistory.child_id.in_(children))\
-            .group_by(models.AppsHistory._name)\
-            .order_by(desc('qty')).all()
+        @safe_db
+        def get_most_popular_apps():
+            """ get most popular apps of the parent children"""
+            return db.session.query(
+                models.AppsHistory._name,
+                func.count(models.AppsHistory.id).label('qty'))\
+                .filter(and_(models.AppsHistory.child_id.in_(children),
+                             models.AppsHistory._opened==True))\
+                .group_by(models.AppsHistory._name)\
+                .order_by(desc('qty')).all()
 
+        histories = get_most_popular_apps()
         histories = [{
             'app_name': h[0],
             'amount': h[1]
-        }
-            for h in histories]
+        } for h in histories]
+
         return histories
